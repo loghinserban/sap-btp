5dcb0bcb8baaeefb117e4909f1e32fe5d2408d4c
Andrei Serban Loghin <andrei.serban.loghin@bmwtechworks.ro>
2026-08-12 08:14:20 +0000

    user view + delails

--- app/userview/webapp/controller/User.controller.js
+++ app/userview/webapp/controller/User.controller.js
@@ -1,10 +1,40 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (Controller, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("userview.controller.User", {
-        onInit() {
+        onInit: function () { },
+
+        onUserSearch: function (oEvent) {
+            var sQuery = oEvent.getParameter("newValue") || oEvent.getParameter("query") || "";
+            var oTable = this.byId("usersTable");
+            var oBinding = oTable.getBinding("items");
+
+            if (!oBinding) { return; }
+
+            if (!sQuery) {
+                oBinding.filter([]);
+                return;
+            }
+
+            var aFilters = [
+                new Filter("firstName", FilterOperator.Contains, sQuery),
+                new Filter("lastName", FilterOperator.Contains, sQuery),
+                new Filter("email", FilterOperator.Contains, sQuery)
+            ];
+
+            oBinding.filter(new Filter({ filters: aFilters, and: false }));
+        },
+
+        onUserPress: function (oEvent) {
+            var oCtx = oEvent.getSource().getBindingContext();
+            if (!oCtx) { return; }
+
+            var sId = oCtx.getProperty("ID");
+            this.getOwnerComponent().getRouter().navTo("RouteDetail", { id: sId });
         }
     });
 });
